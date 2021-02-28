2d074cb1a2c77fae90ae32e6736124bab4b57b84
Marta Nitek <marta.nitek@onet.pl>
2021-02-28 20:25:50 +0100

    Wyszukanie właściwego artykułu

--- js/script.js
+++ js/script.js
@@ -31,7 +31,8 @@ function titleClickHandler(event){
   console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
 
