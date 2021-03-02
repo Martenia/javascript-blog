a67fbdb4c42eca72407798e5fd80cad7232481aa
Marta Nitek <marta.nitek@onet.pl>
2021-03-02 15:44:05 +0100

    Usunięcie zawartości listy linków

--- js/script.js
+++ js/script.js
@@ -36,7 +36,6 @@ function titleClickHandler(event){
 
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
-
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -44,3 +43,30 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
